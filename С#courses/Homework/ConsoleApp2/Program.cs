56820fcd4084c844a265ce0c024f139664aa01e0
michael <ucheba0536@outlook.com>
2018-04-28 19:18:26 +0300

    Домашнее задание 28.04.2018

---
+++ С#courses/Homework/ConsoleApp2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your Name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter your Age: ");
+            short age = short.Parse(Console.ReadLine());
+            DateTime date = DateTime.Now;
+            Console.Write("Your name's ");
+            Console.WriteLine(name);
+            Console.Write("Your age's ");
+            Console.WriteLine(age);
+            Console.Write("Today is  ");
+            Console.WriteLine(date);
+            Console.ReadLine();
+
+
+
+
+
+
+        }
+    }
+}
